c3528e40386e67b22ed3a5c31852b6c4c66fb0f4
Mike Seltzer <mikeyseltz@gmail.com>
2020-07-23 08:09:40 -0700

    working final v1

--- OpenSky.py
+++ OpenSky.py
@@ -9,7 +9,7 @@ import numpy as np
 # s = api.get_states(bbox=(36,40,-118,-124))
 # states = s.states
 
-r = requests.get("https://opensky-network.org/api/states/all")
+r = requests.get("https://opensky-network.org/api/states/all?lamin=36&lomin=-124&lamax=39&lomax=-121")
 
 rest = r.json()
 states = rest['states']
@@ -25,7 +25,7 @@ class Coords:
 
     def __repr__(self):
         if self.lon < 0:
-            return "N " + self.latString + " W " + self.longString
+            return "N " + self.latString + " W " + self.longString[1:]
         else:
             return "N " + self.latString + " E " + self.longString
 
@@ -36,26 +36,30 @@ class Plane:
         self.hdg = hdg
         self.vel = vel
         self.callsign = callsign
-        # self.coords = Coords(self.lat, self.lon)
+        self.coords = Coords(self.lat, self.lon)
 
     def __repr__(self):
         return f"({self.lat}, {self.lon}, {self.hdg}, {self.vel}, {self.callsign})"
 
-    def predict(self, time=60): # this is broken...check equation or use API...
+    def predict(self, time=1):
         r = 3958.8 # radius in miles
         brg = radians(self.hdg)
-        dist = time * self.vel * 2.23694 / 60 / 60
-        startLat = self.lat
-        startLong = self.lon
+        dist = time * self.vel * 2.23694 / 60 # dist traveled in mph
+        startLat = radians(self.lat)
+        startLong = radians(self.lon)
         endLat = asin(sin(startLat)*cos(dist/r) + cos(startLat)*sin(dist/r)*cos(brg))
         endLong = startLong + atan2(sin(brg)*sin(dist/r)*cos(startLat),cos(dist/r)-sin(startLat)*sin(endLat))
         endCoords = Coords(degrees(endLat),degrees(endLong))
         return endCoords
 
 for trk in states:
-    tracks.append(Plane(trk[5], trk[6], trk[10], trk[9], trk[1]))
+    tracks.append(Plane(trk[6], trk[5], trk[10], trk[9], trk[1]))
 
-rando = tracks[np.random.randint(0,len(tracks))]
-print(rando)
-# print("currently at: " + rando.coords)
-print(rando.predict())
+time = input("prediction time in minutes >>>")
+
+for trk in tracks:
+    print()
+    print("     **** " + trk.callsign.strip() + " ****")
+    print("currently at: " + str(trk.coords))
+    print("heading " + str(trk.hdg) + "deg, at " + str(trk.vel) + "m/s")
+    print("will be at: " + str(trk.predict(int(time))))   
