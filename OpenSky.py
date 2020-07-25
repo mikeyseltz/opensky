d4a0f2cc25ca4641041dbe563c1bbda16fb703fd
Ritwik Gupta <ritwikg2004@live.com>
2020-07-24 17:29:43 -0700

    Code style changes

--- OpenSky.py
+++ OpenSky.py
@@ -1,7 +1,7 @@
 # from opensky_api import OpenSkyApi
 
-#organize imports by inlcuded, installed, then alphabetical / plotly own block due to volume
-import json 
+# organize imports by inlcuded, installed, then alphabetical / plotly own block due to volume
+import json
 
 from utils import Coords, Plane
 
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 import requests
 
 import plotly.express as px
-import plotly.graph_objects as go 
+import plotly.graph_objects as go
 
-#classes first, then execute code
-#for modular code, put code in a main function
 
 def main():
+# classes first, then execute code
+# for modular code, put code in a main function
 
     r = requests.get("https://opensky-network.org/api/states/all?lamin=36&lomin=-124&lamax=39&lomax=-121")
     rest = r.json()
@@ -23,7 +23,7 @@ def main():
     tracks = [] # will hold the Planes from the opensky data
 
     for trk in states:
-        if trk[8] == False: #filters tracks that are squawking on the ground
+        if not trk[8]: #filters tracks that are squawking on the ground
             tracks.append(Plane(trk[6], trk[5], trk[10], trk[9], trk[1]))
 
     time = input("prediction time in minutes >>> ")
@@ -33,28 +33,28 @@ def main():
         print(f"      **** {trk.callsign.strip()} ****")
         print(f"currently at:  {str(trk.coords)}")
         print("heading " + str(trk.hdg) + "deg, at " + str(trk.vel) + "m/s")
-        print("will be at: " + str(trk.predict(int(time))))   
-        
+        print("will be at: " + str(trk.predict(int(time))))
+
     paths = [] # will hold plotly info
 
     for i in range(len(tracks)):
         paths.append(
             go.Scattergeo(
-                locationmode = 'USA-states',
-                lon = [tracks[i].lon, tracks[i].predict(int(time)).lon],
-                lat = [tracks[i].lat, tracks[i].predict(int(time)).lat],
-                mode = 'lines+markers',
-                line = dict(width = 1, color = 'red'),
-                marker = {'symbol':  [2,0],'size': 7, 'color':"red"},
-                opacity = float(tracks[i].vel/max(tracks, key = lambda x: x.vel).vel),
-                hoverinfo = 'text',
-                text = tracks[i].callsign
+                locationmode='USA-states',
+                lon=[tracks[i].lon, tracks[i].predict(int(time)).lon],
+                lat=[tracks[i].lat, tracks[i].predict(int(time)).lat],
+                mode='lines+markers',
+                line=dict(width=1, color='red'),
+                marker={'symbol':  [2,0],'size': 7, 'color': "red"},
+                opacity=float(tracks[i].vel/max(tracks, key=lambda x: x.vel).vel),
+                hoverinfo='text',
+                text=tracks[i].callsign
                 ))
 
     layout = go.Layout(
-        autosize = True,
-        showlegend = False,
-        geo = go.layout.Geo(
+        autosize=True,
+        showlegend=False,
+        geo=go.layout.Geo(
             resolution = 50,
             showcoastlines=True,
             scope = 'north america',
